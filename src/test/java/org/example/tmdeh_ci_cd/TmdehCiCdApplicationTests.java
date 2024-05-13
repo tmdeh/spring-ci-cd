f9ee014bbbb9c8b83bd4fbbf910d6ab0e1051cb2
윾승도 <yus174113@gmail.com>
2024-05-13 14:30:27 +0900

    push

--- src/test/java/org/example/tmdeh_ci_cd/TmdehCiCdApplicationTests.java
+++ src/test/java/org/example/tmdeh_ci_cd/TmdehCiCdApplicationTests.java
@@ -1,15 +1,11 @@
 package org.example.tmdeh_ci_cd;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("dev")
 class TmdehCiCdApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
 
 }
